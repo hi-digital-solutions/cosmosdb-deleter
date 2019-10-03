3d197ed8a0521bac657126fd239a39e7d4e6f159
kourtney reynolds <kourtney.reynolds@hidigital.solutions>
2019-10-03 08:49:58 -0700

    Initial project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace cosmosdb_deleter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
